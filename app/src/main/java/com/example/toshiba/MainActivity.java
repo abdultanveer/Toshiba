58f8f58c829b3e801ca845a0dc80ffbb9aacfdfa
Abdul <abdul.tanveer@gmail.com>
2023-03-27 10:47:17 +0530

    handling onclick button

--- app/src/main/java/com/example/toshiba/MainActivity.java
+++ app/src/main/java/com/example/toshiba/MainActivity.java
@@ -2,21 +2,33 @@ package com.example.toshiba;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button  loginButton;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("MainActivity","i am in oncreate");
+        loginButton = findViewById(R.id.btnLogin);
+
+        loginButton.setOnClickListener(this);
     }
 
     public void showToast(View view) {
+        Toast.makeText(this, "welcome to android at toshiba", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClick(View view) {
         Toast.makeText(this, "welcome to android at toshiba", Toast.LENGTH_SHORT).show();
     }
 }
