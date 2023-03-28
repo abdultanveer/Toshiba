a4a9d5e6bad11f228651d96895facfb803311613
Abdul <abdul.tanveer@gmail.com>
2023-03-28 11:10:18 +0530

    start activity for result

--- app/src/main/java/com/example/toshiba/HomeActivity.java
+++ app/src/main/java/com/example/toshiba/HomeActivity.java
@@ -2,23 +2,42 @@ package com.example.toshiba;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
     public static  String TAG = HomeActivity.class.getSimpleName();
+
+    EditText contactEditText;
     TextView homeTv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         //get the intent which started this activity and get the extras from it
-        String data = getIntent().getExtras().getString("nkey");
+       /* String data = getIntent().getExtras().getString("nkey");
         Log.i(TAG,"i am in oncreate of "+TAG + data);
         //taking handle on the textview
         homeTv = findViewById(R.id.tvHome);
-        homeTv.setText(data);
+        homeTv.setText(data);*/
 
+         contactEditText = findViewById(R.id.etContact);
+        Button selectButton = findViewById(R.id.btnSelect);
+        selectButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        String contact = contactEditText.getText().toString();
+        Intent cIntent = new Intent();
+        cIntent.putExtra("ckey",contact);
+        setResult(RESULT_OK,cIntent);
+        finish(); //ends the activity
     }
 }
--- app/src/main/java/com/example/toshiba/MainActivity.java
+++ app/src/main/java/com/example/toshiba/MainActivity.java
@@ -1,20 +1,23 @@
 package com.example.toshiba;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.AlarmClock;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     public static  String TAG = MainActivity.class.getSimpleName();
-    Button  loginButton,dialerButton;
+    Button  loginButton,dialerButton,timerButton,cancelButton;
     EditText nameEditText;
     @SuppressLint("MissingInflatedId")
     @Override
@@ -23,11 +26,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //inflating layout - baloon -- reads the xml and creates corresponding memory variables
         setContentView(R.layout.activity_main);
         Log.i(TAG,"i am in oncreate");
+
         loginButton = findViewById(R.id.btnLogin);
         nameEditText = findViewById(R.id.etName);
         dialerButton = findViewById(R.id.btnDialer);
+        timerButton = findViewById(R.id.btnTimer);
+        cancelButton = findViewById(R.id.btnCancel);
+
+
         loginButton.setOnClickListener(this);
         dialerButton.setOnClickListener(this);
+        timerButton.setOnClickListener(this);
+        cancelButton.setOnClickListener(this);
+
+
     }
 
     public void showToast(View view) {
@@ -44,6 +56,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Intent dIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:9880979732"));
                 startActivity(dIntent);
                 break;
+            case R.id.btnTimer:
+                startTimer("android-toshiba",30);
+                break;
+            case R.id.btnCancel:
+                Intent homeIntent = new Intent(MainActivity.this,HomeActivity.class);
+                startActivityForResult(homeIntent,CONTACT_REQUEST);
+                break;
         }
 
     }
@@ -55,5 +74,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Intent hIntent = new Intent(MainActivity.this, HomeActivity.class);
         hIntent.putExtra("nkey",name);
         startActivity(hIntent);
+    }
+
+
+    public void startTimer(String message, int seconds) {
+        Intent intent = new Intent(AlarmClock.ACTION_SET_TIMER)
+                .putExtra(AlarmClock.EXTRA_MESSAGE, message)
+                .putExtra(AlarmClock.EXTRA_LENGTH, seconds)
+                .putExtra(AlarmClock.EXTRA_SKIP_UI, true);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    int CAMERA_REQUEST = 12;
+    int CONTACT_REQUEST = 34;
+    //request code -- what kind of request
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if(requestCode == CONTACT_REQUEST){
+            TextView resTextView = findViewById(R.id.tvResult);
+            String contact = intent.getExtras().getString("ckey");
+            resTextView.setText(contact);
+
+        }
+
     }
 }
